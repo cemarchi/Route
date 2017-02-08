8f622fff748b5d183cda5610c46fa35a9190ee4c
cemarchi <carlos_marchi@yahoo.com.br>
2017-02-08 21:44:35 -0200

    teste

--- Src/main.py
+++ Src/main.py
@@ -4,7 +4,7 @@ from Router import Router
 if __name__ == '__main__':
     file_path = sys.argv[1]
     input_test = ''
-    print(file_path)
+
     with open(file_path, 'r') as file:
         input_test = file.read()
 
